d85b3786e51c4cf0c6a0690c056f62ef15d57ad7
Md.Ratul Uddin Ashraf <ratulshourov01@gmail.com>
2022-10-22 15:24:16 +0600

    Create customer table into Database

--- database-connection.js
+++ database-connection.js
@@ -1,12 +1,20 @@
 var mysql = require('mysql');
-var con= mysql.createConnection({
+var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: ""
+    password: "",
+    database: "nodejs"
 });
 
-con.connect(function(err) {
-        if(err) throw err;
-        console.log('connected');
+con.connect(function (err) {
+    if (err) throw err;
+    console.log('connected');
+    var sql="CREATE TABLE customers(id int(11) primary key  ,name varchar(255), address varchar(255))";
+    con.query(sql,function(err,result){
+        if(err) {
+            throw err;
+        }
+        console.log('create customer  table');
+    });
 });
 
